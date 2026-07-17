2127ee35c350034b6f827d73ed351535fe5b0c57
Nasreen-Lilly <nasreenhazam@gmail.com>
2026-07-17 17:08:32 +0530

    Fix Advanced Cornea Centre heading overspace

--- update_headers.js
+++ update_headers.js
@@ -12,8 +12,8 @@ const newHeader = newHeaderMatch[0];
 ['about.html', 'services.html', 'contact.html', 'portal.html'].forEach(file => {
   let content = fs.readFileSync(file, 'utf8');
   content = content.replace(headerRegex, newHeader);
-  content = content.replace(/href="css\/components\.css(\?v=\d+)?"/g, 'href="css/components.css?v=30"');
-  content = content.replace(/href="css\/styles\.css(\?v=\d+)?"/g, 'href="css/styles.css?v=30"');
+  content = content.replace(/href="css\/components\.css(\?v=\d+)?"/g, 'href="css/components.css?v=31"');
+  content = content.replace(/href="css\/styles\.css(\?v=\d+)?"/g, 'href="css/styles.css?v=31"');
   fs.writeFileSync(file, content);
   console.log('Updated ' + file);
 });
